a623f3605e274ec271fd7e1601c51f1e4c5715a4
Claudiu Tudor Pavel <claudiu.pavel@hotmail.co.uk>
2024-07-28 14:57:49 +0300

    Add files via upload

---
+++ Assets/collectableSpawnerLogic.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collectableSpawnerLogic : MonoBehaviour {
+
+	public float collectableSpawnTime = 1.0f;
+	public float badCollectableSpawnTime = 2.0f;
+	public bool canSpawnCollectable = true;
+	public bool canSpawnBadCollectable = true;
+
+	float randomGoodX=0.0f;
+	float randomGoodY=0.0f;
+
+	float randomBadX=0.0f;
+	float randomBadY=0.0f;
+
+	float maxX=20.5f;
+	float maxY=9.0f;
+
+	public GameObject goodCollectable;
+	public GameObject badCollectable;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (spawnCollectables());
+		StartCoroutine (spawnBadCollectables());
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator spawnCollectables()
+	{
+		while (canSpawnCollectable == true) 
+		{
+			spawnCollectable ();
+			yield return new WaitForSeconds (collectableSpawnTime);
+		}
+	}
+
+	IEnumerator spawnBadCollectables()
+	{
+		while (canSpawnBadCollectable == true) 
+		{
+			spawnBadCollectable ();
+			yield return new WaitForSeconds (badCollectableSpawnTime);
+		}
+	}
+
+	void spawnCollectable()
+	{
+		randomGoodX = Random.Range (-maxX,maxX);
+		randomGoodY = Random.Range (-maxY, maxY);
+		Instantiate (goodCollectable, new Vector3 (randomGoodX, randomGoodY, 0), Quaternion.identity);
+	}
+
+	void spawnBadCollectable()
+	{
+
+		randomBadX = Random.Range (-maxX,maxX);
+		randomBadY = Random.Range (-maxY, maxY);
+		Instantiate (badCollectable, new Vector3 (randomBadX, randomBadY, 0), Quaternion.identity);
+	}
+}
---
+++ Assets/playerLogic.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerLogic : MonoBehaviour {
+
+	public float playerSpeed = 2000.0f;
+	scoreLogic addToScore;
+
+	// Use this for initialization
+	void Start () {
+		hideMouseCursor ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		movePlayerToMousePos ();
+	}
+
+	void movePlayerToMousePos()
+	{
+		var tempMousePos = Input.mousePosition;
+		tempMousePos.z = transform.position.z - Camera.main.transform.position.z;
+		tempMousePos = Camera.main.ScreenToWorldPoint (tempMousePos);
+		transform.position = Vector3.MoveTowards (transform.position, tempMousePos, playerSpeed * Time.deltaTime);
+	}
+
+	void hideMouseCursor()
+	{
+		Cursor.visible = false;
+	}
+
+	void OnCollisionEnter2D(Collision2D tempCollsion)
+	{
+		if (tempCollsion.gameObject.tag == "goodCollectable") 
+		{
+			callAddToScoreScript ();
+			Destroy (tempCollsion.gameObject);
+		}
+
+		if (tempCollsion.gameObject.tag == "badCollectable") 
+		{
+			resetGame ();
+
+		}
+	}
+
+	void callAddToScoreScript()
+	{
+		addToScore = GameObject.FindGameObjectWithTag ("GUI").GetComponent<scoreLogic> ();
+		addToScore.addToScoreVoid ();
+	}
+
+	void resetGame()
+	{
+		SceneManager.LoadScene ("GameScene");
+	}
+
+}
